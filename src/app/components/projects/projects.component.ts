d83126a672b289b39dd03de9442ef0abf6a617c1
Ruben Garcia <druinha@gmail.com>
2025-07-10 10:36:18 +0200

    more things

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -13,37 +13,37 @@ export class ProjectsComponent {
       title: 'SocialVibes App',
       description:
         'A social media and event platform built for real-time interaction, content sharing, and community engagement.',
-      image: 'https://images.unsplash.com/photo-1581092580505-5c54a5a0cde0',
+      image: '../../../assets/companiesImages/SV3.png',
       tech: ['Ionic', 'Angular', 'Python', 'Firebase', 'Redis', 'Neo4j', 'GCP'],
       category: 'Mobile & Web App',
-      live: '', // or your production URL
+      live: 'https://socialvibes.io/', // or your production URL
       code: '', // or GitHub URL
     },
     {
       title: 'IBDi Medical App',
       description:
         'A clinical support and tracking app for patients with IBD, used in a medical research study.',
-      image: 'https://images.unsplash.com/photo-1588776814546-ec7e6b1ee3b3',
+      image: '../../../assets/companiesImages/ibdi.png',
       tech: ['Ionic', 'Angular', 'Python', 'Firebase', 'GCP'],
       category: 'Healthcare App',
-      live: '',
+      live: 'https://ibdi.se/',
       code: '',
     },
     {
       title: 'Niquita Rengøring Website',
       description:
         'A WordPress-based website for a professional cleaning company with service pages and contact forms.',
-      image: 'https://images.unsplash.com/photo-1581579185169-3c164e8b0306',
+      image: '../../../assets/companiesImages/niquita.png',
       tech: ['WordPress', 'PHP', 'MySQL', 'SEO'],
       category: 'Web Development',
-      live: '',
+      live: 'https://niquitta.dk/',
       code: '',
     },
     {
       title: 'Happy Feet Rengøring',
       description:
         'A modern Angular website for a Danish cleaning service company, optimized for performance and mobile.',
-      image: 'https://images.unsplash.com/photo-1600585154340-be6161a56a0c',
+      image: '../../../assets/images/under_construction.jpg',
       tech: ['Angular', 'TypeScript', 'Tailwind', 'Firebase Hosting'],
       category: 'Web Application',
       live: '',
