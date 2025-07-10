c6c34d065ecdea937f293e3df6a76c44470f8bb5
Ruben Garcia <druinha@gmail.com>
2025-07-10 11:32:16 +0200

    navbar language

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,11 +8,57 @@ import { CommonModule } from '@angular/common';
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  lang: 'en' | 'es' | 'gl' = 'en';
+  langs: ('en' | 'es' | 'gl')[] = ['en', 'es', 'gl'];
+
   isOpen = false;
+  showLangMenu = false;
+
+  navLabels = {
+    en: [
+      'Home',
+      'Services',
+      'Tech Stack',
+      'Pricing',
+      'Projects',
+      'Consulting',
+      'Contact',
+    ],
+    es: [
+      'Inicio',
+      'Servicios',
+      'Tecnologías',
+      'Precios',
+      'Proyectos',
+      'Consultoría',
+      'Contacto',
+    ],
+    gl: [
+      'Inicio',
+      'Servizos',
+      'Tecnoloxías',
+      'Prezos',
+      'Proxectos',
+      'Consultoría',
+      'Contacto',
+    ],
+  };
+
+  switchLang(lang: 'en' | 'es' | 'gl') {
+    this.lang = lang;
+  }
 
   scrollToSection(sectionId: string) {
     const el = document.getElementById(sectionId);
     el?.scrollIntoView({ behavior: 'smooth' });
     this.isOpen = false;
+  }
+
+  getSectionId(label: string): string {
+    return label.toLowerCase().replace(/\s+/g, '-');
+  }
+
+  toggleLangMenu() {
+    this.showLangMenu = !this.showLangMenu;
   }
 }
