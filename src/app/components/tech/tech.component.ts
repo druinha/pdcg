c76c8034c9783065c055fe193bc7bf37df176e28
Ruben Garcia <druinha@gmail.com>
2025-07-17 15:28:40 +0200

    icons ready

--- src/app/components/tech/tech.component.ts
+++ src/app/components/tech/tech.component.ts
@@ -333,4 +333,40 @@ export class TechComponent {
       this.lang = l;
     });
   }
+
+  getIconUrl(techName: string): string {
+    const iconsMap: Record<string, string> = {
+      Ionic: '../../../assets/icons/ionic.png',
+      Capacitor: '../../../assets/icons/capacitor.png',
+      Cordova: '../../../assets/icons/cordova.png',
+      'Android Studio': '../../../assets/icons/android_studio.png',
+      Xcode: '../../../assets/icons/xcode.png',
+      PWA: '../../../assets/icons/pwa.jpg',
+      Figma: '../../../assets/icons/figma.png',
+      'UI/UX Design': '../../../assets/icons/uxui.png',
+      Wireframing: '../../../assets/icons/wireframing.jpeg',
+      Prototyping: '../../../assets/icons/prototype.jpg',
+      'Design Systems': '../../../assets/icons/memo.jpg',
+      Angular: '../../../assets/icons/angular.png',
+      TypeScript: '../../../assets/icons/typescript.png',
+      JavaScript: '../../../assets/icons/javascript.jpg',
+      HTML5: '../../../assets/icons/HTML5_logo_and_wordmark.svg.png',
+      CSS3: '../../../assets/icons/CSS3_logo_and_wordmark.svg.png',
+      'Tailwind CSS': '../../../assets/icons/Tailwind_CSS_Logo.svg.png',
+      Bootstrap: '../../../assets/icons/Bootstrap_logo.svg.png',
+      Firebase: '../../../assets/icons/firebase.png',
+      MySQL: '../../../assets/icons/mysql.png',
+      Redis: '../../../assets/icons/redis.png',
+      Neo4j: '../../../assets/icons/neo4j.jpeg',
+      'RESTful APIs': '../../../assets/icons/restapi.png',
+      'Node.js': '../../../assets/icons/nodejs.png',
+      'Google Cloud': '../../../assets/icons/gcloud.png',
+      'App Store Connect': '../../../assets/icons/appstore.png',
+      'Google Play Console': '../../../assets/icons/googlePlay.jpeg',
+      'Firebase Hosting': '../../../assets/icons/firebase.png',
+      'Apple Developer Console': '../../../assets/icons/appleDev.png',
+      'Google Developer Console': '../../../assets/icons/google-developers.svg',
+    };
+    return 'assets/icons/' + (iconsMap[techName] || 'default-icon.svg');
+  }
 }
