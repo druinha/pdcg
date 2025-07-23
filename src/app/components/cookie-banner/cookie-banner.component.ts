1090914d0e1d8a83e8e291e394830bc65033989b
Ruben Garcia <druinha@gmail.com>
2025-07-23 08:40:37 +0200

    cookies modal ready

--- src/app/components/cookie-banner/cookie-banner.component.ts
+++ src/app/components/cookie-banner/cookie-banner.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { isPlatformBrowser } from '@angular/common';
+import { LanguageService } from '../../services/language.service';
 
 @Component({
   selector: 'app-cookie-banner',
@@ -9,16 +10,40 @@ import { isPlatformBrowser } from '@angular/common';
   styleUrl: './cookie-banner.component.scss'
 })
 export class CookieBannerComponent implements OnInit {
-
+  lang: 'en' | 'es' | 'gl' = 'en';
+  translations = {
+    en: {
+      title: 'Cookie Policy',
+      description: 'We use cookies to improve your experience on our site.',
+      accept: 'Accept',
+      reject: 'Reject',
+    },
+    es: {
+      title: 'Política de Cookies',
+      description: 'Utilizamos cookies para mejorar su experiencia en nuestro sitio.',
+      accept: 'Aceptar',
+      reject: 'Rechazar',
+    },
+    gl: {
+      title: 'Política de Cookies',
+      description: 'Usamos cookies para mellorar a súa experiencia no noso sitio.',
+      accept: 'Aceptar',
+      reject: 'Rexeitar',
+    },
+  };
   showBanner = false;
   isBrowser = false;
 
 
   constructor(
-    @Inject(PLATFORM_ID) private platformId: Object
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private langService: LanguageService,
   ) {
     // Check if the platform is browser
     this.isBrowser = isPlatformBrowser(this.platformId);
+     this.langService.lang$.subscribe((l) => {
+      this.lang = l;
+    });
   }
 
   ngOnInit(): void {
